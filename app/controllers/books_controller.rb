90ada8c61a2aca0f8e26847b44758cc3003d6a84
chinamiii <chinami.2929@icloud.com>
2021-08-21 09:03:45 +0000

    [8/21]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,11 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
-    
+    @user = current_user
+    @book.user_id = current_user.id
+
+
+
   end
 
   def create
@@ -16,6 +20,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.new
     @book = Book.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def edit
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @user = User.new
+    @user = current_user
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image_id)
+  end
+
+end
