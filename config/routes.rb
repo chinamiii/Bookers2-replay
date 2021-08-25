1b01a8080c1952d7baa8945775a13df02551a51f
chinamiii <chinami.2929@icloud.com>
2021-08-25 15:09:03 +0000

    [finish]

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
   resources :users
-  
+
   resources :books do
+    resource :favorites, only: [:create, :destroy]
+
     resources :book_comments, only: [:create, :destroy]
-  end  
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
